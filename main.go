f6b11918a092c23806cfbb7703a9105fbcec09df
Konrad Bachusz <konradbachusz@gmail.com>
2021-12-25 12:03:01 +0000

    updated files

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os"
+	 "os"
 	"strings"
 )
 
@@ -18,17 +18,30 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	originUnit := strings.ToUpper(os.Args[1])
-	if len(os.Args) > 2 {
+	if len(os.Args) !=2 {
 		printError(errInvalidArguments)
 	}
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+         _, err :=fmt.Scanln(&originValue)
+         if err != nil {
+         	printError(errReadingInput)
+		 }
 
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+         _, err = fmt.Scanln(&shouldConvertAgain)
+         if err != nil {
+         	printError(errReadingInput)
+		}
 
-		if shouldConvertAgain != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y"{
 			fmt.Println("Good bye!")
 			break
 		}
@@ -36,8 +49,8 @@ func main() {
 }
 
 func printError(err error) {
-	fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	os.Exit(1)
+	 fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	 os.Exit(1)
 }
 
 func convertToCelsius(value float64) {
